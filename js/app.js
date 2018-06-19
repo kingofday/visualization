e60c296fb36d14e3bcb3b202071415cc6003d873
Kingofday <shahrooz.bazrafshan@gmail.com>
2018-06-19 08:54:50 +0430

    201806190854

--- js/app.js
+++ js/app.js
@@ -14,13 +14,19 @@ $(document).on('ready', function () {
 			var maxMin = opt.split(',');
 			graph.filter.node.min = maxMin[0];
 			graph.filter.node.max = maxMin[1];
+			if(graph.network!=null)
+			{
+				graph.remove();
+				graph.add();
+			}
+
 		}
 	});
 	$('.edges-slider-input').jRange({
 		from: 0,
 		to: 10,
 		step: 1,
-		scale: [0,5,10],
+		scale: [config.edgeRangeMin,5,config.edgeRangeMax],
 		format: '%s',
 		width: $('.edges-slider-input').parent().width(),
 		showLabels: true,
@@ -29,12 +35,22 @@ $(document).on('ready', function () {
 			var maxMin = opt.split(',');
 			graph.filter.edge.min = maxMin[0];
 			graph.filter.edge.max = maxMin[1];
+			if(graph.network!=null)
+			{
+				graph.remove();
+				graph.add();
+			}
 		}
 	});
-	
-    graph.draw();
     $('#test-icon').fontSpy({
-        onLoad: function () { if (graph.network == null) graph.draw(); },
+        onLoad: function () { 
+			if (graph.network == null) 
+			{
+				var fData = graph.filterData();
+				graph.appendData(fData.nodes,fData.edges);
+				graph.init();
+			}			
+		},
         onFail: 'fontFail anotherClass'
     });
 
@@ -50,54 +66,13 @@ $(document).on('ready', function () {
 });
 
 var graph = {
+	network: null,
     currentColIdx: 0,
 	data:{
 		nodes: new vis.DataSet(),
 		edges: new vis.DataSet()
 	},
-	options:{},
-	filter:{
-		node:{
-			min:0,
-			max:10,
-		},
-		edge:{
-			min:0,
-			max:10,
-		}
-	},
-	
-    draw: function () {
-		this.filterData();
-		this.convertData();
-        this.init();
-    },
-    filterData: function () {
-		let nodes = data[this.currentColIdx].nodes.filter(x => x.weight >= this.filter.node.min && x.weight <= this.filter.node.max);
-		let edges = data[this.currentColIdx].edges.filter(x => x.weight >= this.filter.edge.min && x.weight <= this.filter.edge.max && nodes.find(y => y.id == x.from) != null && nodes.find(y => y.id == x.to) != null);
-		if(this.network == null)
-		{
-			this.data.nodes.add(nodes);
-			this.data.edges.add(edges);
-			console.log(this.data.nodes);
-		}
-		else{
-			this.remove(...[
-				this.data.nodes.filter(x=>nodes.find(y=>y.id == x.id) == null),
-				this.data.edges.filter(x=>edges.find(y=>y.id == x.id) == null)
-			]);
-		}
-    },
-	remove:function(nodes,edges){
-		edges.forEach(e=>{
-			this.data.remove({id:e.id});
-		});
-		nodes.forEach(n=>{
-			this.data.remove({id:n.id});
-		});
-	},
-    convertData: function (params) {
-        graph.options = {
+	options:{
             nodes: {
                 shapeProperties: {
                     interpolation: false
@@ -117,8 +92,51 @@ var graph = {
             },
 			interaction:{hover:true},
             groups: {}
-        };
-        this.data.nodes.forEach(function (x) {
+    },
+	filter:{
+		node:{
+			min:0,
+			max:10,
+		},
+		edge:{
+			min:0,
+			max:10,
+		}
+	},
+    filterData: function () {
+		return {
+			nodes:data[this.currentColIdx].nodes.filter(x => x.weight >= this.filter.node.min && x.weight <= this.filter.node.max),
+			edges:data[this.currentColIdx].edges.filter(x => x.weight >= this.filter.edge.min && x.weight <= this.filter.edge.max)
+		};
+    },
+	add:function(){
+		let nodes = data[this.currentColIdx].nodes.filter(x => x.weight >= this.filter.node.min && x.weight <= this.filter.node.max && graph.data.nodes.getIds().find(y=>y == x.id) == null);
+		let edges = data[this.currentColIdx].edges.filter(x => x.weight >= this.filter.edge.min && x.weight <= this.filter.edge.max && graph.data.edges.getIds().find(y=>y == x.id) == null);
+		graph.appendData(nodes,edges);
+	},
+	remove:function(){
+		let nodes = data[this.currentColIdx].nodes.filter(x => x.weight < this.filter.node.min || x.weight > this.filter.node.max);
+		let edges = data[this.currentColIdx].edges.filter(x => x.weight < this.filter.edge.min || x.weight > this.filter.edge.max || nodes.find(y => y.id == x.from) != null || nodes.find(y => y.id == x.to) != null);
+		edges.forEach(e=>{
+			try{
+				this.data.edges.remove({id:e.id});
+			}
+			catch(err){
+			}
+			
+		});
+		console.log(nodes);
+		nodes.forEach(n=>{
+			try{
+				this.data.nodes.remove({id:n.id});
+			}
+			catch(err){
+			}
+			
+		});
+	},
+    appendData: function (nodes,edges) {
+        nodes.forEach(function (x) {
             x['group'] = 'fa_icon' + x.id;
 			if(x.labelColor)
 				x.font.color = x.labelColor;
@@ -136,7 +154,7 @@ var graph = {
 			}
 
         });
-        this.data.edges = this.data.edges.map(x => ({
+        edges = edges.map(x => ({
 			id:x.id,
             from: x.from,
             to: x.to,
@@ -151,70 +169,34 @@ var graph = {
 				size:config.edgeslabelFontSize
 			}
         }));
-
+		console.log(nodes);
+		this.data.nodes.add(nodes);
+		this.data.edges.add(edges);
     },
-    network: null,
     init: function () {
 		console.log(this.data);
         var container = document.getElementById('graph');
         this.network = new vis.Network(container, this.data, this.options);
         //this.network.fit();
 		 this.network.on('hoverNode', function (params2) {
-			 let node = graph.data.nodes.find(x => x.id == params2.node);
+			 let node = data[graph.currentColIdx].nodes.find(x => x.id == params2.node);
 			 if(node.subCol!=null){
 				 graph.network.canvas.body.container.style.cursor = 'pointer';
 			 }
 		});
 		this.network.on('blurNode', function (params2) {
-			 let node = graph.data.nodes.find(x => x.id == params2.node);
+			 let node = data[graph.currentColIdx].nodes.find(x => x.id == params2.node);
 			 if(node.subCol != null){
 				 graph.network.canvas.body.container.style.cursor = 'default';
 			 }
 		});
         this.network.on("selectNode", function (params2) {
-            let node = graph.data.nodes.find(x => x.id == params2.nodes[0]);
-			let edges = data[graph.currentColIdx].edges.filter(x=>(x.from == node.id || x.to == node.id) && graph.data.edges.find(y=>y.id == x.id) == null);
+            let node = data[graph.currentColIdx].nodes.find(x => x.id == params2.nodes[0]);
+			let edges = data[graph.currentColIdx].edges.filter(x=>(x.from == node.id || x.to == node.id) && graph.data.edges.getIds().find(y=>y == x.id) == null);
 			console.log(edges);
-			let nodes = data[graph.currentColIdx].nodes.filter(x=>edges.find(y=>y.from == x.id || y.to == x.id) !=null && graph.data.nodes.find(y=>y.id != x.id) == null);
-			nodes.forEach(x=>{
-				x.font.color = x.labelColor;
-				graph.data.nodes.add(x);
-				if(!graph.options.groups.hasOwnProperty(x.group))
-				{
-					graph.options.groups[x.group] = {
-					shape: 'icon',
-					icon: {
-						face: 'FontAwesome',
-						code: x.icon,
-						size: (x.weight * 10),
-						color: x.color
-						}
-					};
-				}
-
-			});
-			edges.map(x => ({
-				id:x.id,
-				from: x.from,
-				to: x.to,
-				value: x.weight,
-				color: { color: x.color, hover: config.edgesHoverColor, highlight: config.edgesHoverColor },
-				scaling: {
-					max: 9
-				},
-				label:x.label+"-"+x.weight,
-				font:{
-					face:"iransans",
-					size:config.edgeslabelFontSize
-				}
-			})).forEach(x=>{
-				graph.data.edges.add(x);
-			});
-			// if(node.subCol!=null){
-				// graph.currentColIdx = node.subCol;
-				// graph.draw();
-				// $('.node-nav').append('<span>-</span> <a data-idx="'+node.subCol+'">'+node.label+'</a>')
-			// }
+			let nodes = data[graph.currentColIdx].nodes.filter(x=>edges.find(y=>y.from == x.id || y.to == x.id) !=null && graph.data.nodes.getIds().find(y=>y != x.id) == null);
+			graph.appendData(nodes,edges);
+			
         });
     }
 };
